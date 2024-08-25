0265705767cec79eea094d2d0cd6e4942f4fd1bd
itworkmng <itworkllcompany@gmail.com>
2024-08-25 22:50:03 +0700

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { protect, authorize } = require("../middleware/protect");
-const { getCounts, getUserCounts } = require("../controller/admin");
+const { getCounts, getUserCounts, signup } = require("../controller/admin");
 const { adminsignin } = require("../controller/users");
 
 //"/api/v1/admin"
@@ -10,4 +10,5 @@ router
   .route("/counts/:id")
   .get(protect, authorize("superman", "human", "god"), getUserCounts);
 router.route("/signin").post(adminsignin);
+router.route("/signup").post(signup);  
 module.exports = router;

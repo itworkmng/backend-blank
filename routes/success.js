eb28c9bfa17adb267b627f34b8fd9cc90c33b4a4
itworkmng <itworkllcompany@gmail.com>
2024-11-14 18:00:38 +0700

    ⚡️ Updated

--- routes/success.js
+++ routes/success.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 router.route("/").get((req, res) => {
   res.status(200).json({
-    message: "Finally Update 166: 2024/11/13",
+    message: {
+      counter:167,
+      message:"Error 505 fixed"
+    },
     success: true,
   });
 });
